db2c141a7982b91da76f8a38b08ac9e7744d582f
chiyonn <kud0h.chi4@gmail.com>
2024-08-13 12:01:49 +0900

    update

---
+++ nvim.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/chiyonn/nvim.git ~/.config/nvim
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim \
+  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
