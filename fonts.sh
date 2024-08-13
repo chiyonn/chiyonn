a0099c69356ef24b2d1ba0a14a4a80bcebdc6ca6
chiyonn <kud0h.chi4@gmail.com>
2024-08-13 12:12:39 +0900

    create a script to install fonts and enable mozc

--- fonts.sh
+++ fonts.sh
@@ -1,1 +1,13 @@
-sudo pacman -S ttf-font-awesome
+sudo sed -i 's/^# *\(ja_JP.UTF-8 UTF-8\)/\1/' /etc/locale.gen
+
+sudo locale-gen
+
+sudo pacman -S ttf-font-awesome noto-fonts-cjk fcitx5-im fcitx5-mozc
+
+echo "GTK_IM_MODULE=fcitx
+QT_IM_MODULE=fcitx
+XMODIFIERS=@im=fcitx
+" >> $HOME/.xprofile
+
+fcitx5 &
+fcitx5-configtool &
