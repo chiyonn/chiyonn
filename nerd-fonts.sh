c944888c932bea3e93475d32313d424648952272
chiyonn <kud0h.chi4@gmail.com>
2024-08-17 15:03:56 +0900

    update

--- nerd-fonts.sh
+++ nerd-fonts.sh
@@ -1,32 +1,24 @@
 #!/bin/bash
 
-# FiraCode Nerd FontのダウンロードURL
-FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/FiraCode.zip"
-FONT_DIR="$HOME/.local/share/fonts"
-TEMP_DIR=$(mktemp -d)
+sudo pacman -S unzip
 
-# 必要なディレクトリを作成
+# FiraCode Nerd FontのダウンロードURL
+FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/AnonymousPro.zip"
+FONT_DIR="$HOME/.local/share/fonts"
+ZIP_FILE="AnonymicePro.zip"
+
 mkdir -p "$FONT_DIR"
 
-# ZIPファイルをダウンロード
-echo "Downloading FiraCode Nerd Font..."
-wget -O "$TEMP_DIR/FiraCode.zip" "$FONT_URL"
+echo "Downloading Font..."
+wget -O "$ZIP_FILE" "$FONT_URL"
 
-# ZIPファイルを解凍
-echo "Unzipping FiraCode Nerd Font..."
-unzip "$TEMP_DIR/FiraCode.zip" -d "$TEMP_DIR/FiraCode"
+echo "Unzipping Font..."
+unzip "$ZIP_FILE" -d "$FONT_DIR"
 
-# フォントファイルをフォントディレクトリにコピー
-echo "Installing FiraCode Nerd Font..."
-cp "$TEMP_DIR/FiraCode"/*.ttf "$FONT_DIR/"
-
-# フォントキャッシュの更新
 echo "Updating font cache..."
 fc-cache -fv
 
-# 一時ディレクトリとZIPファイルを削除
 echo "Cleaning up..."
-rm -rf "$TEMP_DIR"
+rm -rf "$ZIP_FILE"
 
-echo "FiraCode Nerd Font installation complete!"
-
+echo "Font installation complete!"
